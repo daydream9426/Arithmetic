493af8487d5826c97fd748f9649585954a627642
daydream9426 <1131425512@qq.com>
2017-03-09 17:32:39 +0800

    save files before updating

---
+++ prompt.h
@@ -0,0 +1,23 @@
+#ifndef PROMPT_H_
+#define PROMPT_H_
+
+class prompt
+{
+	public:
+		int setpro(int prompt)
+		{
+			m_iprompt=prompt;
+		}
+		void getpro()
+		{
+			return m_iprompt;
+		}
+		void selectpro(int prompt)
+		{
+		}
+	private:
+		int m_prompt; 
+		
+ } 
+
+#endif
